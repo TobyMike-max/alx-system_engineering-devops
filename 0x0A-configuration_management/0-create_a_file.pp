cb2545703073e8a769d817bb2c34cbdc05f88f42
TobyMike-max <osinubi.michael@gmail.com>
2022-09-19 07:32:55 -0700

    [Modified]: Fixed puppet lint errors

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # create a file in /tmp
 
 file { '/tmp/school':
-        path => '/tmp/school',
-        ensure => 'present',
-        mode => '0744',
-        owner => 'www-data',
-        group => 'www-data',
+        ensure  => 'present',
+        path    => '/tmp/school',
+        mode    => '0744',
+        owner   => 'www-data',
+        group   => 'www-data',
         content => 'I love Puppet'
-	}
+}
