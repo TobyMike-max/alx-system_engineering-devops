3fcca34ada89e88b8a6febfa84feef0695817514
TobyMike-max <osinubi.michael@gmail.com>
2022-10-19 22:23:43 +0100

    [Create]: Push file for task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,15 +5,16 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    user_Id = argv[1]
-    todo = requests.get('https://jsonplaceholder.typicode.com/todos?user_Id={}'
-                        .format(user_Id), verify=False).json()
+    userId = argv[1]
+    user_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'
+        .format(userId), verify=False).json()
     user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                        .format(user_Id)).json()
+                        .format(userId)).json()
     completed_tasks = []
-    for task in todo:
-        if task['completed'] is True:
-            completed_tasks.append(task['title'])
-    print("Employee {} is done with tasks({}/{}):".
-            format(user['name'], len(completed_tasks), len(todo)))
-    print("\n".join("\t {}".format(task) for task in completed_tasks))
+    for todo in user_todos:
+        if todo['completed'] is True:
+            completed_tasks.append(todo['title'])
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user['name'], len(completed_tasks), len(user_todos)))
+    print("\n".join("\t {}".format(todo) for todo in completed_tasks))
